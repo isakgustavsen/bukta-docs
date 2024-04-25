ac2e73b968c6212d93d216668aa048c0d702d404
Isak Wang Gustavsen <69854945+isakgustavsen@users.noreply.github.com>
2024-04-25 02:22:00 +0200

    Added direct links to kinde in config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,4 +6,12 @@ export default defineNuxtConfig({
   sanity: {
     projectId: 'azn1k8lx'
   },
+  kinde: {
+    authDomain: process.env.NUXT_KINDE_AUTH_DOMAIN, 
+    clientId: process. env.NUXT_KINDE_CLIENT_ID, 
+    clientSecret: process.env.NUXT_KINDE_CLIENT_SECRET, 
+    redirectURL: process.env.NUXT_KINDE_REDIRECT_URL, 
+    logoutRedirecturl: process.env.NUXT_KINDE_LOGOUT_REDIRECT_URL,
+    postLoginRedirecturl: process.env.NUXT_KINDE_POST_LOGIN_REDIRECT_URL,
+  },
 })
